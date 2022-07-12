66a73e9635524cceb9d0676730d77a30c5b6eac5
Ig0or <igorprovensi@gmail.com>
2022-07-12 20:48:02 -0300

    feat: changed file names and fixed imports

--- src/controller/post-controller.ts
+++ src/controller/post-controller.ts
@@ -1,13 +1,14 @@
 import { Response, Request } from "express";
-import { MongoRepository } from "../repositories/mongo-repository";
+import { MongoRepository } from "../repositories/mongodb/mongodb-repository";
 
-const b = new MongoRepository();
+const a = new MongoRepository();
 
 class PostController {
     static async getAllPosts(
         request: Request,
         response: Response
     ): Promise<Response> {
+        const b = await a.getPostById("16375272-c15b-4cec-92e4-7ccb138e7f6d");
         return response.send("hello world");
     }
 }
