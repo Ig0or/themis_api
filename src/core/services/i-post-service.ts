12e9348a36d80e9c1557d104953c2e0175e223a4
Ig0or <igorprovensi@gmail.com>
2022-07-16 20:03:06 -0300

    feat: created interface for post service

---
+++ src/core/services/i-post-service.ts
@@ -0,0 +1,11 @@
+import { Post } from "../../domain/models/post_types";
+import { MongoRepository } from "../../repositories/mongodb/mongodb-repository";
+
+interface IPostService {
+    mongoRepository: MongoRepository;
+    getAllPosts(): Promise<Array<Post>>;
+    getPostById(postId: string): Promise<Post>;
+    createPost(title: string, body: string, userId: string): string;
+}
+
+export { IPostService };
--- src/services/post-service.ts
+++ src/services/post-service.ts
@@ -1,18 +1,20 @@
-import { MongoRepository } from "../repositories/mongodb/mongodb-repository";
+import { IPostService } from "../core/services";
+import { MongoRepository } from "../repositories/mongodb";
+import { Post } from "../domain/models/";
 
-class PostService {
+class PostService implements IPostService {
     mongoRepository: MongoRepository;
     constructor(mongoRepository: MongoRepository) {
         this.mongoRepository = mongoRepository;
     }
 
-    async getAllPosts() {
+    async getAllPosts(): Promise<Array<Post>> {
         const posts = await this.mongoRepository.getAllPosts();
 
         return posts;
     }
 
-    async getPostById(postId: string) {
+    async getPostById(postId: string): Promise<Post> {
         const post = await this.mongoRepository.getPostById(postId);
 
         return post;
