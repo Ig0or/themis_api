f1cc11069b1af620d0e04946e0399b7696f40b77
Ig0or <igorprovensi@gmail.com>
2022-07-28 23:09:54 -0300

    feat: configurated index to parsing json

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import { postRouter } from "./routers";
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("/posts", postRouter);
 
 app.listen(port, () => {
