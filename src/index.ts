1d08a395c4bfc6a26e7a639f151869fbfc8289ef
Ig0or <igorprovensi@gmail.com>
2022-08-04 17:23:09 -0300

    feat: included users routter at app

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { postRouter } from "./routers";
+import { postRouter, userRouter } from "./routers";
 
 const app = express();
 const port = 3000;
@@ -8,6 +8,7 @@ const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/posts", postRouter);
+app.use("/users", userRouter);
 
 app.listen(port, () => {
     console.log(port);
