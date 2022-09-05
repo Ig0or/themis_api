3fa038fbad0dfa71d8d4797a712174f57052dda7
Ig0or <igorprovensi@gmail.com>
2022-09-04 23:36:49 -0300

    feat: created function to build and run server

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,47 @@
 // Third Party
-import express from "express";
+import chalk from "chalk";
+import express, { Express } from "express";
+import figlet from "figlet";
 
 // Local
-import { postRouter, userRouter } from "@routers/index";
+import { postRouter } from "@routers/post/post-router";
 
-const app = express();
-const port = 3000;
+function applyBaseRoutes(server: Express): void {
+  server.use("/posts", postRouter);
+}
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use("/posts", postRouter);
-app.use("/users", userRouter);
+function buildServer(): Express {
+  const server = express();
 
-app.listen(port, () => {
-    console.log(port);
-});
+  server.use(express.json());
+  server.use(express.urlencoded({ extended: true }));
 
-export { app };
+  applyBaseRoutes(server);
+
+  return server;
+}
+
+function runServer(): void {
+  const server = buildServer();
+  const port = process.env.SERVER_PORT ?? 3000;
+
+  server.listen(port, () => {
+    console.log(`Server is running at port ${port}`);
+    console.log(
+      chalk.redBright(
+        figlet.textSync("Themis", {
+          horizontalLayout: "default",
+          verticalLayout: "default",
+          width: 100,
+          whitespaceBreak: true,
+        })
+      )
+    );
+  });
+}
+
+function main(): void {
+  runServer();
+}
+
+main();
