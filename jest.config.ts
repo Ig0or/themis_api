99898d5ba5f32eb754a042b96e6b94574cbb2b07
Ig0or <igorprovensi@gmail.com>
2022-09-12 13:39:59 -0300

    chore: test config

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/test"],
+  moduleNameMapper: {
+    "^@domain(.*)$": "<rootDir>/src/domain/$1",
+    "^@infrastructure(.*)$": "<rootDir>/src/infrastructure/$1",
+    "^@services(.*)$": "<rootDir>/src/services/$1",
+    "^@repositories(.*)$": "<rootDir>/src/repositories/$1",
+    "^@root(.*)$": "<rootDir>/$1",
+  },
+  collectCoverageFrom: ["src/**/*.ts"],
+};
